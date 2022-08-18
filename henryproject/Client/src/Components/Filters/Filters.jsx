bfc2a11ce35cc26c02864ad835cfb9a5e69e5d45
Robles Lautaro <lautaro0121@gmail.com>
2022-08-18 19:57:48 -0300

    inicio componente filters

---
+++ henryproject/Client/src/Components/Filters/Filters.jsx
@@ -0,0 +1,29 @@
+export default function Filters () {
+
+
+    return (
+        <div>
+            <div>
+                <span> Filter by </span>
+                <br></br>
+                <br></br>
+                <span> Genres </span>
+                <br></br>
+                <br></br>
+                <span> tags </span>
+                <br></br>
+                <br></br>
+            </div>
+            <div>
+                <div>
+                    <span> Sort </span>
+                    <br></br>
+                    
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
--- henryproject/Client/src/Components/Home/Home.jsx
+++ henryproject/Client/src/Components/Home/Home.jsx
@@ -12,15 +12,15 @@ export default function Home () {
         
         <div className="home-primary-videogames">
           <button>left</button>
-          <div>
+          <div className='home-card'>
 
             <div className='home-img-primary'>
-             <span> Ya Disponible !</span>
+             <span  > Ya Disponible !</span>
              <button> COMPRAR </button>
             </div>
 
             <div className="home-show-img-secundary">
-              <span> MINECRAFT </span>
+              <span> Titulo </span>
               <img src='https://i0.wp.com/codigoespagueti.com/wp-content/uploads/2022/07/videojuegos-ma%CC%81s-exitosos.jpg?fit=1280%2C720&quality=80&ssl=1' 
               alt='imagen secundaria'
               className="home-img-secundary"
@@ -29,17 +29,90 @@ export default function Home () {
               alt='imagen secundaria'
               className="home-img-secundary"
               ></img>
-              Ya disponible !!
+              (resumen, generos, plataforma, tag etc)
               <button> Learn more... </button>
             </div>
           </div> 
           <button> right</button>
         </div>
 
-        <div>
-          
+        <div className="home-primary-videogames">
+          <button>left</button>
+          <div className='home-card'>
+
+            <div className='home-img-primary'>
+             <span  > Ya Disponible !</span>
+             <button> COMPRAR </button>
+            </div>
+
+            <div className="home-show-img-secundary">
+              <span> Titulo </span>
+              <img src='https://i0.wp.com/codigoespagueti.com/wp-content/uploads/2022/07/videojuegos-ma%CC%81s-exitosos.jpg?fit=1280%2C720&quality=80&ssl=1' 
+              alt='imagen secundaria'
+              className="home-img-secundary"
+              ></img>
+              <img src='https://www.eluniverso.com/resizer/JmanqxXU51w1Bsa0X0Nd9iz1cSM=/660x371/smart/filters:quality(70)/cloudfront-us-east-1.images.arcpublishing.com/eluniverso/ZYX6AFU7OBH7PBXUTCXQEICJ7E.jpg' 
+              alt='imagen secundaria'
+              className="home-img-secundary"
+              ></img>
+              (resumen, generos, plataforma, tag etc)
+              <button> Learn more... </button>
+            </div>
+          </div> 
+          <button> right</button>
         </div>
 
+        <div className="home-primary-videogames">
+          <button>left</button>
+          <div className='home-card'>
+
+            <div className='home-img-primary'>
+             <span  > Ya Disponible !</span>
+             <button> COMPRAR </button>
+            </div>
+
+            <div className="home-show-img-secundary">
+              <span> Titulo </span>
+              <img src='https://i0.wp.com/codigoespagueti.com/wp-content/uploads/2022/07/videojuegos-ma%CC%81s-exitosos.jpg?fit=1280%2C720&quality=80&ssl=1' 
+              alt='imagen secundaria'
+              className="home-img-secundary"
+              ></img>
+              <img src='https://www.eluniverso.com/resizer/JmanqxXU51w1Bsa0X0Nd9iz1cSM=/660x371/smart/filters:quality(70)/cloudfront-us-east-1.images.arcpublishing.com/eluniverso/ZYX6AFU7OBH7PBXUTCXQEICJ7E.jpg' 
+              alt='imagen secundaria'
+              className="home-img-secundary"
+              ></img>
+              (resumen, generos, plataforma, tag etc)
+              <button> Learn more... </button>
+            </div>
+          </div> 
+          <button> right</button>
+        </div>
+
+        <div className="home-primary-videogames">
+          <button>left</button>
+          <div className='home-card'>
+
+            <div className='home-img-primary'>
+             <span  > Ya Disponible !</span>
+             <button> COMPRAR </button>
+            </div>
+
+            <div className="home-show-img-secundary">
+              <span> Titulo </span>
+              <img src='https://i0.wp.com/codigoespagueti.com/wp-content/uploads/2022/07/videojuegos-ma%CC%81s-exitosos.jpg?fit=1280%2C720&quality=80&ssl=1' 
+              alt='imagen secundaria'
+              className="home-img-secundary"
+              ></img>
+              <img src='https://www.eluniverso.com/resizer/JmanqxXU51w1Bsa0X0Nd9iz1cSM=/660x371/smart/filters:quality(70)/cloudfront-us-east-1.images.arcpublishing.com/eluniverso/ZYX6AFU7OBH7PBXUTCXQEICJ7E.jpg' 
+              alt='imagen secundaria'
+              className="home-img-secundary"
+              ></img>
+              (resumen, generos, plataforma, tag etc)
+              <button> Learn more... </button>
+            </div>
+          </div> 
+          <button> right</button>
+        </div>
 
     </div>
     )
