0f3c38c5b3130ac1c5db81a4ddef32de947b742e
12frankporx <f.s.b.rojas@gmail.com>
2022-08-18 15:07:45 -0500

    User-Modelo

--- henryproject/api/src/models/users.js
+++ henryproject/api/src/models/users.js
@@ -32,7 +32,7 @@ module.exports = (sequelize) => {
     lastName:  {
       type: DataTypes.BOOLEAN,
       defaultValue: false,
-    },
+    }, 
     image:  {
       type: DataTypes.STRING,
       defaultValue: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"
