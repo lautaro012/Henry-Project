cd41a5adafbcdd8bfaa946e98e16450dea999307
12frankporx <f.s.b.rojas@gmail.com>
2022-08-18 13:19:02 -0500

    cambio

--- henryproject/api/src/db.js
+++ henryproject/api/src/db.js
@@ -6,7 +6,6 @@ const Games = require('./models/Games');
 const Genres = require('./models/Genres');
 const Platforms = require('./models/Platforms');
 const Comments = require('./models/Comments');
-const Users = require('../src/models/Users');
 const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
