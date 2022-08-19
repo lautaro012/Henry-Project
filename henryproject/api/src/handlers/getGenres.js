08125fc7d52f937a7a99871781c7e0e66920edba
12frankporx <f.s.b.rojas@gmail.com>
2022-08-19 13:16:06 -0500

    Se modificó las rutas genres y plataforms

--- henryproject/api/src/handlers/getGenres.js
+++ henryproject/api/src/handlers/getGenres.js
@@ -7,7 +7,7 @@ const getGenres = async ()  =>{
     const allGenres = await Genres.findAll();
     if(allGenres.length===0){
       const genresInfo= ((await axios.get(`https://api.rawg.io/api/genres?key=${API_KEY}`)).data.results).map((el => el.name)); 
-      console.log(genresInfo);
+      // console.log(genresInfo);
       genresInfo.map(async (el) => {
         await Genres.findOrCreate({
           where: {name: el}
