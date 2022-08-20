06275194b1e412f1ac4bf0080b5c06f8a8f1490b
juandavid84 <juandavid614@hotmail.com>
2022-08-20 15:05:51 -0500

    informacion en about

--- henryproject/Client/src/Components/About_Us/About.jsx
+++ henryproject/Client/src/Components/About_Us/About.jsx
@@ -1,17 +1,22 @@
 import { Link } from "react-router-dom";
 import React from "react";
 import '../About_Us/About.css'
+import lautaro from "./lautaro.jpeg"
 
 export default function About() {
     return (
         <div className="About">
-            <h1>About this page</h1>
-            <p>Juan David</p>
+            <h1>Nuestro proyecto</h1>
             <p>
-                HLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-                in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+             Visite nuestro Ecommerce en donde va a encontrar las mejores ofertas
+             de videojuegos para que pueda elegir la diversion ideal.
             </p>
+            <div className="name">
+                <h3>Lautaro</h3>
+                <p>Frontend</p>
+                <a href='https://ar.linkedin.com/in/lautaro-robles-57a5ba242?trk=people-guest_people_search-card'>LinkedIn</a>
+                <img id="imgl" src={lautaro} alt="lautaro"></img>
+            </div>
 
             <div>
                 <Link to='/home'>
