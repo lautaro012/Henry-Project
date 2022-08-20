1fafd4a6834de71248bf5206eea2cb0bb2b7bbe1
12frankporx <f.s.b.rojas@gmail.com>
2022-08-20 14:19:17 -0500

    probando

--- henryproject/api/src/routes/videogames.js
+++ henryproject/api/src/routes/videogames.js
@@ -1,13 +1,13 @@
 const { Router } = require("express")
-const { getAllVideoGames, getGamesByName } = require("../handlers/getAllVideoGames")
+const { getAllVideoGames } = require("../handlers/getAllVideoGames")
 const { findGameById } = require("../handlers/getGamesDetail")
 const { createNewGame } = require("../handlers/postNewGame")
 // const { getVideogames } = require("../handlers/routeGetVideogamesApi")
 
 const router = Router()
 
-router.get("/", getGamesByName)
- router.get("/:gameId", findGameById)
+router.get("/", getAllVideoGames)
+router.get("/:gameId", findGameById)
 router.post("/", createNewGame)
 
 module.exports = router
