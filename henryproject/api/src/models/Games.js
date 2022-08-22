185f6a2412604d8ca487baa5ca4d8bd8b6938078
lubermudezz <bermudezluciana@hotmail.com>
2022-08-22 02:43:51 -0300

    db games image and not null fixed

--- henryproject/api/src/models/Games.js
+++ henryproject/api/src/models/Games.js
@@ -27,24 +27,19 @@ module.exports = (sequelize) => {
     },
     image: {
       type: DataTypes.TEXT,
-      allowNull: false,
       defaultValue: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
     },
     videoTrailer: {
       type: DataTypes.TEXT,
       allowNull:true
     },
-    image:{
-      type:DataTypes.STRING,
-      allowNull:false
-    },
+
     tags: {
       type: DataTypes.TEXT,
       allowNull: true
     },
     createdInDb:{
         type: DataTypes.BOOLEAN,
-        allowNull:false,
         defaultValue:true,
     }
 
