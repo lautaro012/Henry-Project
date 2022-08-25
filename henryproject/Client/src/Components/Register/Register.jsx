4c114ba79452d81b0b22fb10632e4e29ed37915c
lubermudezz <bermudezluciana@hotmail.com>
2022-08-25 18:46:11 -0300

    se agrega post de nuevo usuario

--- henryproject/Client/src/Components/Register/Register.jsx
+++ henryproject/Client/src/Components/Register/Register.jsx
@@ -1,27 +1,57 @@
 import React from 'react'
+import { useState } from 'react'
 import { useDispatch } from 'react-redux'
+import { postNewUser } from '../../redux/Actions/Index'
 import "./Register.css"
 
 const Register = () => {
     const dispatch= useDispatch()
 
+    const [newUser, setNewUser] = useState({
+        userName: "",
+        name: "",
+        lastName: "",
+        mail: "",
+        adress: ""
+
+    })
+
+    function handleChange(e) {
+        e.preventDefault(e)
+        if(e.target.name) {
+            let usuario = {...newUser}
+            usuario[e.target.name] = e.target.value
+            setNewUser({...newUser, [e.target.name]: e.target.value})
+        } else {
+            return console.log('falta enviar datos')
+        }
+
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        dispatch(postNewUser(newUser))
+        alert('Te has registrado correctamente')
+    }
+
+
 
   return (
     <div className='divFormRegister'>
-        <form className='formRegister'>
-            <label>UserName:</label>
-            <input></input>
-            <label>Name:</label>
-            <input></input>
-            <label>LastName:</label>
-            <input></input>
-            <label>Mail:</label>
-            <input></input>
+        <form className='formRegister' onSubmit={(e) => handleSubmit(e)} >
+            <label type="text" >UserName:</label>
+            <input name="userName" onChange={(e) => handleChange(e)}></input>
+            <label type="text" >Name:</label>
+            <input name="name" onChange={(e) => handleChange(e)} ></input>
+            <label type="text" >LastName:</label>
+            <input name="lastName" onChange={(e) => handleChange(e)} ></input>
+            <label type="text" >Mail:</label>
+            <input name="mail" onChange={(e) => handleChange(e)} ></input>
             <label>Profile Photo:</label>
-            <input type="file"></input>
+            <input name="image" type="file"></input>
             <label>Adress:</label>
-            <input></input>
-            <button type='submit' className='buttonFormRegister'> REGISTER </button>
+            <input name="adress" type="text"  onChange={(e) => handleChange(e)}></input>
+            <button type="submit" className='buttonFormRegister'> REGISTER </button>
 
         </form>
     </div>
