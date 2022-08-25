677ac358eb59bf07109e970711acc66f9c89cb4d
lubermudezz <bermudezluciana@hotmail.com>
2022-08-25 17:17:58 -0300

    post de crear usuario

--- henryproject/api/src/handlers/postNewUser.js
+++ henryproject/api/src/handlers/postNewUser.js
@@ -1,29 +1,26 @@
-// const {Users} = require('../db')
+const {Users} = require('../db')
 
-// const createNewUser = async (req, res) => {
-//     let { name, lastName, mail, adress, userName, image } = req.body;
+const createNewUser = async (req, res) => {
+    let { name, lastName, mail, adress, userName, image } = req.body;
 
-//     if (!userName || !name || !lastName || !mail) return res.status(404).send("Falta enviar datos obligatorios")
-//     try {
-//         let newUser = await Users.create({
-//             name,
-//             lastName,
-//             mail,
-//             adress,
-//             userName,
-//             image
-//         })
+    if (!userName || !name || !lastName || !mail) return res.status(404).send("Falta enviar datos obligatorios")
+    try {
+        let newUser = await Users.create({
+            name,
+            lastName,
+            mail,
+            adress,
+            userName,
+            image
+        })
 
-//         res.send(newUser)
+        res.send(newUser)
 
-//     } catch (error) {
-//         return res.status(404).send("Error en alguno de los datos provistos")
-//     }
-// }
+    } catch (error) {
+        return res.status(404).send("Error en alguno de los datos provistos")
+    }
+}
 
 
-
-// module.exports = {
-//     createNewUser
-// }
+module.exports = {createNewUser}
   
