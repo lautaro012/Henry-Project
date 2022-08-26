a2ebe6dc2cb1be35644945c41b53690941a1b7a7
Robles Lautaro <lautaro0121@gmail.com>
2022-08-26 01:23:33 -0300

    deploy front

--- henryproject/Client/src/index.js
+++ henryproject/Client/src/index.js
@@ -5,8 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
 import { store } from './redux/Store/Index'
+import axios from 'axios'
+import dotenv from "dotenv";
 
-
+dotenv.config();
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
