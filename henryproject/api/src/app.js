8045b415918050a4330722927c76a773968fef14
Robles Lautaro <lautaro0121@gmail.com>
2022-08-29 15:11:44 -0300

    *

--- henryproject/api/src/app.js
+++ henryproject/api/src/app.js
@@ -27,7 +27,7 @@ server.use(cookieParser());
 server.use(morgan('dev'));
 
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Origin', 'true'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
