8bedad0d1647ed9bb46b23a05e1a3728e65c26e3
TheSerch07 <jejog50@gmail.com>
2022-08-31 12:43:33 -0500

    delete and put completed

--- henryproject/api/src/handlers/putUser.js
+++ henryproject/api/src/handlers/putUser.js
@@ -4,11 +4,11 @@ const putUser = async (req, res) => {
     let { id_name } = req.params
     let { name, lastName, password, mail, address, admin, userName, image } = req.body
     try {
-        let response = await Users.update(
+        await Users.update(
             { name, lastName, password, mail, address, admin, userName, image }, 
             { where: {id_name}
         })
-        return res.status(200).json(response)
+        return res.status(200).json("User updated")
     } catch(err) {
         console.log(err)
     }
