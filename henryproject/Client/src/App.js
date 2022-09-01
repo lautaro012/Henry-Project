7f0b7c9b61053b7dce90950093d6154d3ea23881
Robles Lautaro <lautaro0121@gmail.com>
2022-09-01 17:26:12 -0300

    deploy FUNCIONABA TODO MENOS LOCALSTORAGE

--- henryproject/Client/src/App.js
+++ henryproject/Client/src/App.js
@@ -44,29 +44,29 @@ function App() {
   
 
   useEffect(() => {
-    //   const getUser = async () => {
-    //   fetch(`${REACT_APP_API}/auth/success`, {
-    //     method: "GET",
-    //     // credentials: "include",
-    //     headers: {
-    //     Accept: "application/json", 
-    //     "Content-Type": "application/json",
-    //     "Access-Control-Allow-Credentials": true
-    //     },
-    //   }).then((response) => {
-    //     if(response.status === 200) {
-    //       console.log('entra a response')
-    //       return response.json()};
-    //     throw new Error('authentication has been failed')
-    //   }).then(resObject => {
-    //     setUserLogged(true)
-    //     localStorage.setItem('user', JSON.stringify(resObject))
-    //     setUser(resObject.user)
-    //   }).catch(err => {
-    //     console.log(err)
-    //   })
-    // }
-    //   getUser()
+      const getUser = async () => {
+      fetch(`${REACT_APP_API}/auth/success`, {
+        method: "GET",
+        // credentials: "include",
+        headers: {
+        Accept: "application/json", 
+        "Content-Type": "application/json",
+        // "Access-Control-Allow-Credentials": true
+        },
+      }).then((response) => {
+        if(response.status === 200) {
+          console.log('entra a response')
+          return response.json()};
+        throw new Error('authentication has been failed')
+      }).then(resObject => {
+        setUserLogged(true)
+        localStorage.setItem('user', JSON.stringify(resObject))
+        setUser(resObject.user)
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+      getUser()
   }, [])
 
 
