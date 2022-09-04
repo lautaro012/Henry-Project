4fced15fbecb5e82c959282818700673ceb343b2
juandavid84 <juandavid614@hotmail.com>
2022-09-04 08:37:55 -0500

    ultimos cambios

--- henryproject/Client/src/Components/Admin/Admin.jsx
+++ henryproject/Client/src/Components/Admin/Admin.jsx
@@ -44,6 +44,8 @@ export default function Admin() {
 
     function showGame(e) {
         e.preventDefault()
+        console.log(e.target.value)
+        console.log(showVideoGame(e.target.value))
         dispatch(showVideoGame(e.target.value))
 
     }
