bb92bcf5b0c0366d7adbc1abd1b38ce8e52d19f6
juandavid84 <juandavid614@hotmail.com>
2022-09-05 18:47:42 -0500

    actualizaciones

--- henryproject/Client/src/Components/SendNews/SendNews.jsx
+++ henryproject/Client/src/Components/SendNews/SendNews.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const SendNews = () => {
   return (
-    <div>SendNews</div>
+    <div>
+    <h1>Ofertas para mails registrados</h1>
+    <select>
+    <option value={1}>mail 1</option>
+              <option value={2}>mail 2</option>
+              <option value={3}>mail 3</option>
+              <option value={4}>mail 4</option>
+              <option value={5}>mail 5</option>
+              <option value={6}>mail 6</option>
+    </select>
+    </div>
   )
 }
 
