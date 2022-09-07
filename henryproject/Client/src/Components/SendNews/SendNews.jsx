d2e6e347e4dbddf991c2b6028df6e00f55354a3c
Robles Lautaro <lautaro0121@gmail.com>
2022-09-07 19:47:06 -0300

    sendnews

--- henryproject/Client/src/Components/SendNews/SendNews.jsx
+++ henryproject/Client/src/Components/SendNews/SendNews.jsx
@@ -4,13 +4,14 @@ import { useEffect } from 'react'
 import './SendNews.css'
 import { getAllMailsNews } from '../../redux/Actions/Index'
 import { useState } from 'react'
-
-
+import axios from 'axios'
 
 const SendNews = () => {
  const dispatch= useDispatch()
  const mailsNews= useSelector(state=>state.mailsNews)
  const [mails, setMails] = useState([])
+ const [input, setInput] = useState('')
+ const [loading, setLoading] = useState(false)
  console.log('aaaaaaaaaaaaaaaaaaa',mailsNews)
 
 useEffect(() => {
@@ -28,23 +29,34 @@ function onChangeEmail(ev){
   if(!mails?.includes(ev.target.value) && ev.target.value !== "All") {
     setMails([...mails, ev.target.value])
   }
-
 }
 
 
 function handleSubmit(ev) {
   ev.preventDefault()
+  setLoading(true)
+  axios.post('/sendNewsletter', [mails, input] ).then(resp => resp.data)
+  .then(resp => {
+    setLoading(false)
+    console.log(resp)
+  })
+  .catch(err => {
+    console.log(err)
+    setLoading(false)
+  })
+}
 
+function handleChange(e) {
+  setInput(e.target.value)
+  console.log(input)
 }
 
   return (
-
     <div className='firstDivFormSendNews'>
     <h3 className='titleSendNewOffer'>Send your newsletter:</h3>
     <form onSubmit={(ev) => handleSubmit(ev)}> 
       <div className='divFormSendNews'>
-      <textarea placeholder="Write and email" rows="10" cols="70"></textarea>
-
+      <textarea placeholder="Write and email" rows="10" cols="70" onChange={e => handleChange(e)}></textarea>
       <label>Emails:</label>
       <select name="mails" onChange={(ev)=>onChangeEmail(ev)} className='buttonSendNewsForm'>
       <option value="All">select user:</option>
@@ -60,15 +72,18 @@ function handleSubmit(ev) {
 
         </div>
       </div>
-      <button type='submit' className='buttonSendNewsForm' >Send Email</button>
+      {
+        loading ?
+        <button disabled={true} type='submit' className='buttonSendNewsForm'> Loading </button>
+        :
+        <button type='submit' className='buttonSendNewsForm' >Send Email</button>
+      }
       </div>
     </form>
     {/* <img src={}></img> */}
 
     </div>
 
-
   )
-      }
-
+}
 export default SendNews
