54bd48464c94aa75ba196fb5cede53905262a553
lubermudezz <bermudezluciana@hotmail.com>
2022-09-08 00:59:39 -0300

    pointer navbar

--- henryproject/api/src/handlers/getMails.js
+++ henryproject/api/src/handlers/getMails.js
@@ -4,13 +4,13 @@ const getMails= async() => {
     console.log("hola")
     const users= await Users.findAll()
     console.log(users)
-//    try {
-//     const users= await Users.findAll();
-//     console.log(users)
-//     return users;
+    try {
+     const users= await Users.findAll();
+     console.log(users)
+     return users;
     
-//     } catch (error) {
-//         console.log('error en traer usuarios',error)
-//     }
+     } catch (error) {
+         console.log('error en traer usuarios',error)
+     }
 }
 module.exports= getMails
